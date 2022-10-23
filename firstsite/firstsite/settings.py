11f678ef7d4db0f1d1a391e2654cf0439bc664c0
yelo1916 <yelo1916@gmail.com>
2022-10-23 15:42:00 +0800

    register models

--- firstsite/firstsite/settings.py
+++ firstsite/firstsite/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-# article, customers, customerOrder, customerCart are not on the book 
+# article, customers, customerOrder, customerCart are not in the book 
 
 INSTALLED_APPS = [
     'django.contrib.admin',
--- firstsite/polls/models.py
+++ firstsite/polls/models.py
@@ -1,13 +1,18 @@
+import datetime
+
 from email.policy import default
 from django.db import models
-import datetime
+from django.utils import timezone
 
 # Create your models here.
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        
     def __str__(self) -> str:
         return self.question_text
 
