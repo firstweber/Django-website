1b5e6c329529c378063f2314eb7dafce3c310018
yelo1916 <yelo1916@gmail.com>
2023-01-12 21:54:05 +0800

    add detail.html

--- firstsite/polls/views.py
+++ firstsite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,render
 from django.http import Http404, HttpResponse, HttpResponseBadRequest
 from django.template import loader
 
@@ -20,7 +20,7 @@ from .models import Question
 #    }
 #    return HttpResponse(template.render(context, request))
 
-# Using shortcuts.render
+# Using shortcut: render
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
@@ -33,8 +33,21 @@ def officaltutor(request):
 def test_error(anything):
     return HttpResponse(status=404)
 
+#def detail(request, question_id):
+#    return HttpResponse("You're looking at question %s." % question_id)
+
+# Using shortcut: render
+#def detail(request, question_id):
+#    try:
+#        question = Question.objects.get(pk=question_id)
+#    except Question.DoesNotExist:
+#        raise Http404("Question does not exist")
+#    return render(request, 'polls/detail.html', {'question': question})
+
+# Using shortcut: get_object_or_404()
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at question %s."
