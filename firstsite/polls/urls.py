9ee5280b2987f668097d06d59fcc50bdd182965a
yelo1916 <yelo1916@gmail.com>
2023-01-17 22:01:32 +0800

    polls index update

--- firstsite/polls/urls.py
+++ firstsite/polls/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('offical', views.officaltutor, name='tutor'),
     path('error', views.test_error, name='error'),
     path('<int:question_id>/', views.detail, name= 'detail'),  # <int:> only accept type:int. ex: /polls/5/
+    #path('specifics/<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),  # ex: /polls/5/results/
     path('<int:question_id>/vote/', views.vote, name='vote'),  # ex: /polls/5/vote/
+    
 ]
