f76efd58c314c31b99f7f45420b8416b677e85a7
yelo1916 <yelo1916@gmail.com>
2023-02-10 17:14:25 +0800

    create results.html & add urls, views

--- firstsite/polls/urls.py
+++ firstsite/polls/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('offical', views.officaltutor, name='tutor'),
@@ -8,6 +10,5 @@ urlpatterns = [
     path('<int:question_id>/', views.detail, name= 'detail'),  # <int:> only accept type:int. ex: /polls/5/
     #path('specifics/<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),  # ex: /polls/5/results/
-    path('<int:question_id>/vote/', views.vote, name='vote'),  # ex: /polls/5/vote/
-    
+    path('<int:question_id>/vote/', views.vote, name='vote'),  # ex: /polls/5/vote/    
 ]
