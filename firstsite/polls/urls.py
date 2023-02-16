3049014d91a47eac4adf86577de1c85254235014
yelo1916 <yelo1916@gmail.com>
2023-02-16 16:40:09 +0800

    update to class-view

--- firstsite/polls/urls.py
+++ firstsite/polls/urls.py
@@ -4,11 +4,14 @@ from . import views
 app_name = 'polls'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('offical', views.officaltutor, name='tutor'),
-    path('error', views.test_error, name='error'),
-    path('<int:question_id>/', views.detail, name= 'detail'),  # <int:> only accept type:int. ex: /polls/5/
-    #path('specifics/<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),  # ex: /polls/5/results/
+    # path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('offical/', views.officaltutor, name='tutor'),
+    path('error/', views.test_error, name='error'),
+    # path('<int:question_id>/', views.detail, name= 'detail'),  # <int:> only accept type:int. ex: /polls/5/
+    # path('specifics/<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # path('<int:question_id>/results/', views.results, name='results'),  # ex: /polls/5/results/
+    path('<int:pk>/results', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),  # ex: /polls/5/vote/    
 ]
--- firstsite/polls/views.py
+++ firstsite/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404,render
 from django.http import Http404, HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
 
 
 from .models import Question, Choice
@@ -81,3 +82,21 @@ def vote(request, question_id):
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,))) # reverse() return like : "/polls/3/results/"
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+    
+    
