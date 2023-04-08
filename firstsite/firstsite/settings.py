8c3c2b094c9009849508473a72be872f3e3d408f
yelo1916 <yelo1916@gmail.com>
2023-04-08 15:58:47 +0800

    nothing change

--- firstsite/firstsite/settings.py
+++ firstsite/firstsite/settings.py
@@ -121,11 +121,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    '/var/www/static/',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
-    '/var/www/static/',
-]
