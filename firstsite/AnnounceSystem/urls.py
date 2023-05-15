cb05fa2c370533e6169f74583657de0be3f7506c
yelo1916 <yelo1916@gmail.com>
2023-05-15 15:42:10 +0800

    add newsadd views & url , update newsadd.html

--- firstsite/AnnounceSystem/urls.py
+++ firstsite/AnnounceSystem/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('adminshow/', views.adminShow, name='showData'),
     path('adminshow/<str:pageindex>/', views.adminShow, name='showData'),
+    path('addnews/', views.newsadd, name='newsadd'),
 ]
